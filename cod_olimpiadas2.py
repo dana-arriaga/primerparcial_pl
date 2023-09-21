ad6b6e4d55c0f5ffc6aed0e879019cf77732edc1
dana-arriaga <145025986+dana-arriaga@users.noreply.github.com>
2023-09-21 08:05:50 -0600

    Create cod_olimpiadas2.py

---
+++ cod_olimpiadas2.py
@@ -0,0 +1,13 @@
+def suma(a,b):
+  x = a + b
+  return x 
+
+def resta(a,b):
+  x= a - b
+  return x 
+  
+print("Dame el primer numero:")
+a = int(input())
+print("Dame el segundo numero")
+b=int(input())
+print("la suma da")
