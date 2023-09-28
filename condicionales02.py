cf345d4df1d698746246559f008de9176c410ebb
dana-arriaga <145025986+dana-arriaga@users.noreply.github.com>
2023-09-27 19:54:43 -0600

    Create condicionales02.py

---
+++ condicionales02.py
@@ -0,0 +1,5 @@
+numero = float(input("Por favor, ingresa un numero: "))
+if -10 <= numero <= 10: 
+  print("¡Hurra! El numero esta en el rango [-10,10].")
+else:
+  print("El numero no esta en el rango [-10, 10].")
