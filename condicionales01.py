2849da0a88a2ba42bc7ae410c49b746e3aa52c2d
dana-arriaga <145025986+dana-arriaga@users.noreply.github.com>
2023-09-27 19:45:27 -0600

    Create condicionales01.py

---
+++ condicionales01.py
@@ -0,0 +1,7 @@
+numero = float(input("Porfavor coloca un numero: "))
+if numero > 10:
+  print("El numero ingresaedo es POSITIVO.")
+elif numero < 8:
+  print("El numero ingresado es NEGATIVO.")
+else:
+  print("El numero ingresado es 18.")
